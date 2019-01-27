13692dd37fe6be3ab61b65ba8593637d517f83f6
umbrashia <shantanu34@outlook.com>
2019-01-27 16:23:04 +0530

    Installing Firebase

--- code-repositories/home/src/app/app.component.ts
+++ code-repositories/home/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'home';
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
 }
--- code-repositories/home/src/app/app.module.ts
+++ code-repositories/home/src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyASg2b9NQ0cyvT8Oe92IlVgKF9LpKT0nEA",
+  authDomain: "universe-1490765561619.firebaseapp.com",
+  databaseURL: "https://universe-1490765561619.firebaseio.com",
+  projectId: "universe-1490765561619",
+  storageBucket: "universe-1490765561619.appspot.com",
+  messagingSenderId: "1043952116464"
+};
+firebase.initializeApp(config);
 
 @NgModule({
   declarations: [
